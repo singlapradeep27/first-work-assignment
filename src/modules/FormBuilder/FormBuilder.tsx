370102d54bba06ab3168adc4ae416ea1448f5fd7
Pardeep Singla <singlapradeep27@gmail.com>
2025-03-04 21:09:12 +0530

    fixed options and drag-drap

--- src/modules/FormBuilder/FormBuilder.tsx
+++ src/modules/FormBuilder/FormBuilder.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import FieldItem from "./FieldItem/FieldItem";
 import { FormField } from "../../types/formField";
 import { useService } from "../../services/formService";
@@ -13,149 +13,168 @@ import ConfirmationDialog from "../../components/atoms/Dialog/ConfirmationDialog
 import FullScreenLoader from "../../components/atoms/FullScreenLoader/FullScreenLoader";
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import { IconButton } from "@mui/material";
+import { debounce } from "lodash";
 
 const FormBuilder: React.FC<{}> = () => {
-  const { state, dispatch } = useFormContext();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const { deleteField, upsertField, updateFieldOrder } = useService();
-  const [removalableId, setRemovalableId] = useState<string>();
+	const { state, dispatch } = useFormContext();
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { deleteField, upsertField, updateFieldOrder } = useService();
+	const [removalableId, setRemovalableId] = useState<string>();
+	// update order
+	const autoSave = useCallback(
+		debounce(async (fieldIds: string[]) => {
+			try {
+				await updateFieldOrder(fieldIds);
+			} catch (e) {
+			}
+		}, 2000),
+		[],
+	);
+	useEffect(() => {
+		const ids = state.fields.map((field) => field.id).filter(Boolean);
+		autoSave.cancel();
+		autoSave(ids as string[]);
+		return () => {
+			autoSave.cancel();
+		};
+	}, [state.fields]);
 
-  const addField = () => {
-    const newField: FormField = {
-      label: "",
-      key: "",
-      id: `UI-${uuidv4()}`,
-    };
-    dispatch({
-      type: "ADD_FIELD",
-      payload: newField,
-    });
-  };
+	const addField = () => {
+		const newField: FormField = {
+			label: "",
+			key: "",
+			id: `UI-${uuidv4()}`,
+		};
+		dispatch({
+			type: "ADD_FIELD",
+			payload: newField,
+		});
+	};
 
-  const removeField = (id: string) => {
-    setRemovalableId(id);
-  };
+	const removeField = (id: string) => {
+		setRemovalableId(id);
+	};
 
-  const duplicateField = async (id: string) => {
-    const idx = state.fields.findIndex((field) => field.id === id);
-    const field = JSON.parse(JSON.stringify(state.fields[idx]));
-    setIsLoading(true);
-    if (id.includes("UI")) {
-      field.id = `UI-${uuidv4()}`;
-      dispatch({
-        type: "ADD_FIELD",
-        payload: field,
-      });
-    } else {
-      delete field.id;
-      try {
-        const newField = await upsertField(field);
-        dispatch({
-          type: "ADD_FIELD",
-          payload: newField,
-        });
-      } catch (e) {
-        //handle Error here
-      }
-    }
-    setIsLoading(false);
-  };
+	const duplicateField = async (id: string) => {
+		const idx = state.fields.findIndex((field) => field.id === id);
+		const field = JSON.parse(JSON.stringify(state.fields[idx]));
+		setIsLoading(true);
+		if (id.includes("UI")) {
+			field.id = `UI-${uuidv4()}`;
+			dispatch({
+				type: "ADD_FIELD",
+				payload: field,
+			});
+		} else {
+			delete field.id;
+			try {
+				const newField = await upsertField(field);
+				dispatch({
+					type: "ADD_FIELD",
+					payload: newField,
+				});
+			} catch (e) {
+				//handle Error here
+			}
+		}
+		setIsLoading(false);
+	};
 
-  const deleteFieldPermanantaly = async () => {
-    try {
-      setRemovalableId("");
-      setIsLoading(true);
-      if (removalableId && !removalableId?.includes("UI-")) {
-        await deleteField(removalableId);
-      }
-      removalableId &&
-        dispatch({
-          type: "REMOVE_FIELD",
-          payload: removalableId,
-        });
-      await updateFieldOrder(
-        state.fields
-          .map((field) => field.id)
-          .filter((id) => id !== removalableId) as string[],
-      );
-    } catch (e) {
-      //handle Error here
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	const deleteFieldPermanantaly = async () => {
+		try {
+			setRemovalableId("");
+			setIsLoading(true);
+			if (removalableId && !removalableId?.includes("UI-")) {
+				await deleteField(removalableId);
+			}
+			removalableId &&
+				dispatch({
+					type: "REMOVE_FIELD",
+					payload: removalableId,
+				});
+			await updateFieldOrder(
+				state.fields
+					.map((field) => field.id)
+					.filter((id) => id !== removalableId) as string[],
+			);
+		} catch (e) {
+			//handle Error here
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  const onFieldsOrderUpdate = async (fields: FormField[]) => {
-    dispatch({
-      type: "SET_FIELDS",
-      payload: fields,
-    });
-    setIsLoading(true);
-    try {
-      await updateFieldOrder(fields.map((field) => field.id) as string[]);
-    } catch (e) {
-      //handle Error here
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	const onFieldsOrderUpdate = async (fields: FormField[]) => {
+		dispatch({
+			type: "SET_FIELDS",
+			payload: fields,
+		});
+		setIsLoading(true);
+		try {
+			await updateFieldOrder(fields.map((field) => field.id) as string[]);
+		} catch (e) {
+			//handle Error here
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  return (
-    <div className="form-builder">
-      <h2 className="form-builder__title">Form Builder</h2>
-      <DndWrapper
-        items={state.fields}
-        idKey="id"
-        onDragEnd={onFieldsOrderUpdate}
-      >
-        {state.fields.map((field, index) => (
-          <SortableItem
-            key={field.id}
-            id={field.id!}
-            dragHandle={
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  width: "100%",
-                  position: "absolute",
-                  top: "-10px",
-                }}
-              >
-                <IconButton
-                  sx={{
-                    cursor: "grab",
-                  }}
-                >
-                  <DragIndicatorIcon sx={{ transform: "rotate(90deg)" }} />
-                </IconButton>
-              </div>
-            }
-          >
-            <FieldItem
-              key={field.id}
-              fieldData={field}
-              removeField={removeField}
-              duplicateField={duplicateField}
-            />
-          </SortableItem>
-        ))}
-      </DndWrapper>
-      <Button onClick={addField} variant="contained">
-        Add Question
-      </Button>
-      <ConfirmationDialog
-        open={!!removalableId}
-        title="Confirm Delete"
-        description="Are you sure you want to delete this item?"
-        onConfirm={deleteFieldPermanantaly}
-        onCancel={() => setRemovalableId("")}
-      />
-      {<FullScreenLoader open={isLoading} />}
-      <BootStrap />
-    </div>
-  );
+	return (
+		<div className="form-builder">
+			<h2 className="form-builder__title">Form Builder</h2>
+			<DndWrapper
+				items={state.fields}
+				idKey="id"
+				onDragEnd={onFieldsOrderUpdate}
+			>
+				{state.fields.map((field, index) => (
+					<SortableItem
+						key={field.id}
+						id={field.id!}
+						dragHandle={
+							<div
+								style={{
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+									width: "100%",
+									position: "absolute",
+									top: "-10px",
+								}}
+							>
+								<IconButton
+									sx={{
+										cursor: "grab",
+									}}
+								>
+									<DragIndicatorIcon sx={{ transform: "rotate(90deg)" }} />
+								</IconButton>
+							</div>
+						}
+					>
+						<FieldItem
+							key={field.id}
+							fieldData={field}
+							removeField={removeField}
+							duplicateField={duplicateField}
+						/>
+					</SortableItem>
+				))}
+			</DndWrapper>
+			<Button onClick={addField} variant="contained">
+				Add Question
+			</Button>
+			<ConfirmationDialog
+				open={!!removalableId}
+				title="Confirm Delete"
+				description="Are you sure you want to delete this item?"
+				onConfirm={deleteFieldPermanantaly}
+				onCancel={() => setRemovalableId("")}
+			/>
+			{<FullScreenLoader open={isLoading} />}
+			<BootStrap />
+		</div>
+	);
 };
 
 export default FormBuilder;
